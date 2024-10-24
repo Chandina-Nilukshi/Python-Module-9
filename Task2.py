2eed0d12c56ff4c06f2ad4bf7c399c42ec8922bc
Chandina-Nilukshi <chandinn@metropolia.fi>
2024-10-24 17:05:33 +0530

    Initial Commit

---
+++ Task2.py
@@ -0,0 +1,34 @@
+class Car:
+    def __init__(self, registration_number, maximum_speed, current_speed=0, travelled_distance=0):
+        self.registration_number = registration_number
+        self.maximum_speed = maximum_speed
+        self.current_speed = current_speed
+        self.travelled_distance = travelled_distance
+
+    def accelerate(self, speed):
+        self.current_speed += speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+
+    def output(self):
+        print(f"Registration_number: {self.registration_number}\n" 
+              f"maximum_speed: {self.maximum_speed}\n" 
+              f"current_speed: {self.current_speed}\n"
+              f"travelled_distance: {self.travelled_distance}")
+
+
+car = Car("ABC-123", 142)
+
+
+
+car.accelerate(30)
+car.accelerate(70)
+car.accelerate(50)
+print(car.current_speed)
+
+car.accelerate(-200)
+print(car.current_speed)
+
+
+
+
