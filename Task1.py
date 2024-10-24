e37da6c21ee8bba70120d68d753c6cc8c28d6720
Chandina-Nilukshi <chandinn@metropolia.fi>
2024-10-24 20:37:43 +0530

    Initial Commit

--- Task1.py
+++ Task1.py
@@ -5,15 +5,11 @@ class Car:
         self.current_speed = current_speed
         self.travelled_distance = travelled_distance
 
-    def output(self):
-        print(f"Registration_number: {self.registration_number}\n" 
-              f"maximum_speed: {self.maximum_speed}\n" 
-              f"current_speed: {self.current_speed}\n"
-              f"travelled_distance: {self.travelled_distance}")
-
 car = Car("ABC-123", "142km/h")
 
-car.output()
-
+print(f"Registration Number: {car.registration_number}"
+      f"\nMaximum Speed: {car.maximum_speed}"
+      f"\nCurrent Speed: {car.current_speed}"
+      f"\nTravelled Distance: {car.travelled_distance}")
 
 
---
+++ Task4.py
@@ -0,0 +1,50 @@
+import random
+from tabulate import tabulate
+
+
+class Car:
+    def __init__(self, registration_number, maximum_speed, current_speed=0, travelled_distance=0):
+        self.registration_number = registration_number
+        self.maximum_speed = maximum_speed
+        self.current_speed = current_speed
+        self.travelled_distance = travelled_distance
+
+    def accelerate(self, speed):
+        self.current_speed += speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+
+    def drive(self, hours):
+        distance = self.current_speed * hours
+        return distance
+
+
+car1 = Car("ABC-1", random.randint(100, 200))
+car2 = Car("ABC-2", random.randint(100, 200))
+
+while car1.travelled_distance < 10000:
+    if car1.current_speed < car1.maximum_speed:
+        car1.accelerate(random.randint(10, 15))
+    distance = car1.drive(1)
+    if distance + car1.travelled_distance <= 10000:
+        car1.travelled_distance += distance
+    else:
+        break
+
+
+while car2.travelled_distance < 10000:
+    if car2.current_speed < car2.maximum_speed:
+        car2.accelerate(random.randint(10, 15))
+    distance = car2.drive(1)
+    if distance + car2.travelled_distance <= 10000:
+        car2.travelled_distance += distance
+    else:
+        break
+
+print(tabulate([['car1', car1.registration_number, car1.current_speed, car1.travelled_distance, car1.maximum_speed],
+               ['car2', car2.registration_number, car2.current_speed, car2.travelled_distance, car2.maximum_speed]],
+               headers=['car', 'registration_number', 'current_speed', 'travelled_distance', 'maximum_speed']))
+
+
